ba6bd1cc172052863dcd05a3ecb8d99f030219fb
makowiec <ptosiek@poczta.fm>
2015-11-10 13:39:32 +0100

    - wxWidgets - pierwsze okno;

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "simple.h"
+
+IMPLEMENT_APP(MyApp)
+
+bool MyApp::OnInit()
+{
+    Simple *simple = new Simple(wxT("Pierwsze okno"));
+    simple->Show(true);
+
+    return true;
+}
