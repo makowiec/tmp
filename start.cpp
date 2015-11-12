d03404beb5321420dc3d46be5600600dfc53a7e9
makowiec <ptosiek@poczta.fm>
2015-11-12 10:02:08 +0100

    - zmiana parametrów funkcji new wxButton; - dodanie akcji kliknięcia dla dwóch przycisków; - wywołanie funkcji dla przycisków North i South;

--- start.cpp
+++ start.cpp
@@ -1,6 +1,5 @@
 #include "start.h"
 #include <iostream>
-#include <wx/event.h>
 
 Start::Start(const wxString& title)
     : wxFrame(NULL, wxID_ANY, title, wxDefaultPosition, wxSize(300, 215))
@@ -14,12 +13,14 @@ Start::Start(const wxString& title)
     wxPanel *panel = new wxPanel(this, wxID_ANY);
 
 // tworznie przycisku
-    wxButton *button_start1 = new wxButton(panel, wxID_EXIT, wxT("North"), wxPoint(30, 30), wxSize(100, 75));
-    wxButton *button_start2 = new wxButton(panel, wxID_EXIT, wxT("South"), wxPoint(170, 30), wxSize(100, 75));
-    wxButton *button_start3 = new wxButton(panel, wxID_EXIT, wxT("Quit"), wxPoint(100, 135), wxSize(100, 50));
+    wxButton *button_start_N = new wxButton(panel, wxID_ANY, wxT("North"), wxPoint(30, 30), wxSize(100, 75));
+    wxButton *button_start_S = new wxButton(panel, wxID_ANY, wxT("South"), wxPoint(170, 30), wxSize(100, 75));
+    wxButton *button_start_Q = new wxButton(panel, wxID_EXIT, wxT("Quit"), wxPoint(100, 135), wxSize(100, 50));
 
 // akcja - klikniecie
     Connect(wxID_EXIT, wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(Start::OnQuit));
+    button_start_N -> Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(Start::OnNorth));
+    button_start_S -> Connect(wxEVT_COMMAND_BUTTON_CLICKED, wxCommandEventHandler(Start::OnSouth));
 
 // ustawienie zacznika na okreslony przycisk - taka ramka do przejscia tabulatorem
     //button_1->SetFocus();
@@ -29,8 +30,18 @@ Start::Start(const wxString& title)
     Centre();
  }
 
-// funkcja zamykajaca okno
+// funkcja zamykajaca okno - przycisk utton_start_Q
 void Start::OnQuit(wxCommandEvent & WXUNUSED(event))
 {
     Close(true);
+}
+// funkcja dla przucisku button_start_N
+void Start::OnNorth(wxCommandEvent & WXUNUSED(event))
+{
+
+}
+//funkcja dla przycisku button_start_S
+void Start::OnSouth(wxCommandEvent & WXUNUSED(event))
+{
+
 }
--- start.h
+++ start.h
@@ -1,3 +1,6 @@
+#ifndef START_H
+#define START_H
+
 #include <wx/wx.h>
 
 class Start : public wxFrame
@@ -5,5 +8,9 @@ class Start : public wxFrame
 public:
     Start(const wxString& title);
 
+
     void OnQuit(wxCommandEvent & event);
+    void OnNorth(wxCommandEvent & event);
+    void OnSouth(wxCommandEvent & event);
 };
+#endif
