4a8d262bead5b58989f7b611cb7ea2a1fb2b3e0a
makowiec <ptosiek@poczta.fm>
2015-11-12 10:22:26 +0100

    - dodanie plików rwin.cpp i rwin.h do obsługi zadań z North;

---
+++ rwin.h
@@ -0,0 +1,14 @@
+#ifndef RWIN_H
+#define RWIN_H
+
+/*
+============================================================================
+Name 		: rwin.cpp
+Author 		: makowiec
+Version 	: 0.1
+Copyright 	: priv
+Description : Okno dla danych z North ;)
+============================================================================
+*/
+
+#endif
