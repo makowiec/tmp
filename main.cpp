0507ec22247a41d72620a6acf5a0ecd280214846
makowiec <ptosiek@poczta.fm>
2015-11-27 09:27:02 +0100

    - otwierane pliku; - weryfikacja dostępu do pliku;

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
+#include <fstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    std::ifstream plik;
+    plik.open( "C:\\temp\\1.txt", std::ios::in );
+
+    if( plik.good() == true )
+{
+    std::cout << "Uzyskano dostep do pliku!" << std::endl;
+
+//tu zaraz coœ porobimy
+
+
+    plik.close();
+
+}
+
+
+    else std::cout << "Dostep do pliku zostal zabroniony!" << std::endl;
+
     return 0;
 }
